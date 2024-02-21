65cc9b51e2697e32054b9f40bd49135839d140d2
Alexangel <alexangelb@gmail.com>
2024-02-21 11:20:59 -0300

    changes on main and models

--- models.py
+++ models.py
@@ -2,16 +2,9 @@ from pydantic import BaseModel
 import numpy as np
 
 class Prediction_Input(BaseModel):
-    flag : str
-    id : int
-    input_parameter : str
-
+    db_id : int
+    additional_comment : str
 
 class Prediction_Output(BaseModel):
-    flag : str
-    id : int
-    input_parameter_used : str    #Prediction_Input.input_parameter
-    prediction : list
-
-
-## this is donesaksak
+    db_id : int
+    additional_comment : str    #Prediction_Input.input_parameter
